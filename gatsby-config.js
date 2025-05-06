c499ba2cce5be05491c82e17663eb20df2655531
euvely <102321531+euvely@users.noreply.github.com>
2025-05-07 00:07:52 +0900

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     // Site URL for when it goes live
-    siteUrl: `https://monumental-chimera-b1ace3.netlify.app/`,
+    siteUrl: `https://euvely.github.io`,
     // Your Name
     name: 'Ryan Fitzgerald',
     // Main Site Title
